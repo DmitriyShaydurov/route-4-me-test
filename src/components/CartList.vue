1a1475ae57b9c65eca9f685445bc950ea13ac150
Dmitriy Shaidurov <dmitriy.shaydurov@gmail.com>
2023-06-17 17:52:22 +0700

    fix bugs

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto p-4">
     <header class="flex justify-between items-center mb-4">
       <h1 class="text-2xl font-bold">Корзина</h1>
-      <a href="/" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+      <a href="./" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
         <svg class="h-6 w-6 mr-2 fill-current" viewBox="0 0 24 24">
           <path d="M21 5H7.16l2.92-2.92A.996.996 0 1 0 8.29.71L2.71 6.29A.996.996 0 0 0 2 7v13c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V7c0-.55-.45-1-1-1zm-9 14H4v-2h8v2zm6 0h-4v-2h4v2zM4 10V8h14v2H4zm0 4v-2h14v2H4zm6 3H4v-2h6v2zm6 0h-4v-2h4v2z"></path>
         </svg>
